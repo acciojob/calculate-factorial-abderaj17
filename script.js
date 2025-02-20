81333ce337b42ccfd72728823e90047eef646307
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-20 19:00:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 //your JS code here. If required.
 function calculateFactorial() {
-	
+	let number = prompt("Enter a number");
+	number = Number(number);
+	let factorial = 1;
+
+		for(let i = 1; i <= number; i++){
+		factorial *= i;
+	}
+ alert(`The factorial of ${number} is ${factorial}`);
 }
+calculateFactorial()
